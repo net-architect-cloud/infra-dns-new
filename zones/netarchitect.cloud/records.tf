5bd1be05f868d955403eea47dd8d433a47c2f372
Kevin Allioli <kevin.allioli@infomaniak.com>
2025-12-29 15:42:23 +0100

    Fix NS record target syntax and remove subdomain delegation examples
    Corrected trailing dots in ns_1 target (dns110.ovh.net.. to dns110.ovh.net.) and removed example NS records for subdomain delegation (subdomain_ns and subdomain_ns2).

--- zones/netarchitect.cloud/records.tf
+++ zones/netarchitect.cloud/records.tf
@@ -18,7 +18,7 @@ resource "ovh_domain_zone_record" "ns_1" {
   zone      = data.ovh_domain_zone.this.name
   fieldtype = "NS"
   subdomain = "@"
-  target    = "dns110.ovh.net.."
+  target    = "dns110.ovh.net."
   ttl       = 0
 }
 resource "ovh_domain_zone_record" "ns_2" {
@@ -44,24 +44,6 @@ resource "ovh_domain_zone_record" "grafana" {
   subdomain = "grafana"
   target    = "135.125.175.79"
   ttl       = 0
-}
-
-# NS record for subdomain delegation
-resource "ovh_domain_zone_record" "subdomain_ns" {
-  zone      = data.ovh_domain_zone.this.name
-  fieldtype = "NS"
-  subdomain = "subdomain"
-  target    = "ns1.ovh.net."
-  ttl       = 3600
-}
-
-# NS record for multiple nameservers
-resource "ovh_domain_zone_record" "subdomain_ns2" {
-  zone      = data.ovh_domain_zone.this.name
-  fieldtype = "NS"
-  subdomain = "subdomain"
-  target    = "ns2.ovh.net."
-  ttl       = 3600
 }
 
 # Note: OVH provider does not have a "refresh" resource - changes are applied automatically
